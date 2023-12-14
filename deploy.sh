af9cee895431c5881aee46aa8dd374852d5da731
Ahrorbek Davronov <axrorbektatu@gmail.com>
2023-12-14 13:20:10 +0500

    deploy github pages

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+rm -rf dist
+
+git add -A
+git commit -m 'deploy github pages'
+git branch -D gh-pages
+git branch gh-pages
+git push origin master
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy background'
+ 
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:axrorbekdf/axrorbekdf.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:axrorbekdf/metro-masala.git master:gh-pages
+
+cd -
---
+++ vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    publicPath: '/metro-masala/'
+  }
