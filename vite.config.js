471ceba7062ad33b8988ecf490a7d86da47d431a
Ahrorbek Davronov <axrorbektatu@gmail.com>
2023-12-14 16:34:36 +0500

    deploy github pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/metro-masala", 
+  base: "/davronov-nodirbek-ekologiya", 
   plugins: [
     vue(),
   ],
